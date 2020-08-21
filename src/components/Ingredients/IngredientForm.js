aad8f4240adeef2f4b370483e20cb34035ca0dc0
Your Name <aew11111d@gmail.com>
2020-08-21 19:09:18 +0300

    useState() test

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+
   const submitHandler = event => {
     event.preventDefault();
+    props.onAddIngredient({title: enteredTitle, amount: enteredAmount})
     // ...
   };
 
@@ -15,11 +20,17 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" defaultValue={enteredTitle}
+             onChange={(event) => {
+               setEnteredTitle(event.target.value)
+             }}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" defaultValue={enteredAmount}
+             onChange={(event) => {
+                setEnteredAmount(event.target.value)
+               }}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList'
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+
+  const [userIngredients, setUserIngredients] = useState([])
+
+  const addIngredienthandler = (ingredient) => {
+    setUserIngredients((prevIngredients) => {
+      return [...prevIngredients, {id: Math.random().toString(), ...ingredient}] 
+    })
+  }
+  
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={(ingredient) => addIngredienthandler(ingredient)}/>
 
       <section>
         <Search />
+        <IngredientList ingredients={userIngredients} 
+          onRemoveItem={(id) => {
+            const updatedIngredients = userIngredients.filter((ingredient) => {
+              return ingredient.id !== id;
+            })
+            setUserIngredients(updatedIngredients);
+          }
+        }/>
         {/* Need to add list here! */}
       </section>
     </div>
